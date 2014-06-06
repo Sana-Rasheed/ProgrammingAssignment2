628c69945570997fdb17bb339adff6afefbdf6f3
Sana Rasheed <sana.rasheed.14aug@gmail.com>
2014-06-06 18:28:42 +0500

    Programming Assignment is up for Github Desktop

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL # set null value
+    set <- function(y) { # set the value of the matrix
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x  # get the value of the matrix
+    setinverse <- function(inverse) inv <<- inverse # set the value of the inverse
+    getinverse <- function() inv  # get the value of the inverse
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) # listing the values
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    #getting value from cache
+    inv <- x$getinverse() # get value from cache
+    if(!is.null(inv)) { # if not null
+        message("getting cached data.") #display message
+        return(inv) # return value
+    }
+    #value is not in cache, so calculate
+    data <- x$get() # get matrix from the cache
+    inv <- solve(data) # this will inverse the square matrix
+    x$setinverse(inv) # set calculated value to cache
+    inv # return value
 }
